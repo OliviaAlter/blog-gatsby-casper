039af153b327061626f0707ac6d981844cee3dfb
OliviaAlter <Innocentbystander0123@gmail.com>
2022-05-24 11:50:33 +0700

    Updated worker

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,5 @@
 require('prismjs/themes/prism-tomorrow.css')
 
 export const onServiceWorkerUpdateReady = () => {
-  const answer = window.confirm(
-    'This blog\'s client assets have recently been updated. Reload to display the latest version?'
-  )
-  if (answer === true)
     window.location.reload()
 }
